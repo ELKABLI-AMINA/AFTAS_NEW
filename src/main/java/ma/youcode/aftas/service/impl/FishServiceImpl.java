9f2b6dbc56617bf2a5b80067cf1a0896cba4c184
amina <amina.elkabli1@gmail.com>
2023-12-15 21:38:03 +0100

    fix MemberService

--- src/main/java/ma/youcode/aftas/service/impl/FishServiceImpl.java
+++ src/main/java/ma/youcode/aftas/service/impl/FishServiceImpl.java
@@ -2,12 +2,14 @@ package ma.youcode.aftas.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import ma.youcode.aftas.entities.Fish;
+import ma.youcode.aftas.exception.ResourceAlreadyExistsException;
 import ma.youcode.aftas.repository.FishRepository;
 import ma.youcode.aftas.service.IFishService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class FishServiceImpl implements IFishService {
     @Override
     public Fish save(Fish fish) {
         if(fishRepository.findByName(fish.getName()).isPresent()){
-            throw new IllegalArgumentException("Fish with name " + fish.getName() + " already exists");
+            throw new ResourceAlreadyExistsException("Fish with name " + fish.getName() + " already exists");
         }
         return fishRepository.save(fish);
     }
@@ -33,7 +35,7 @@ public class FishServiceImpl implements IFishService {
         if(existingFish.isPresent()){
             fishRepository.delete(existingFish.get());
         }else {
-            throw new IllegalArgumentException("Fish with ID " + id + " not found");
+            throw new NoSuchElementException("Fish with ID " + id + " not found");
         }
 
     }
